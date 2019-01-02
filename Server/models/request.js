d67ec8c27b9c87f63e031b4b34db6d23b9872ba8
Robert Cao <ch.robert.26@gmail.com>
2019-01-02 21:36:39 +0800

    order schema

--- Server/models/request.js
+++ Server/models/request.js
@@ -1,17 +1,41 @@
+/* 
+{
+    landlord: '', // id of the landlord
+    leaser: '', // id of the leaser
+    asset: '',  // id of the asset
+    status: [ // enum of string
+      'Pending',
+      'Ongoing',
+      'Ended',
+      'Issue',
+    ],
+    startDate: date,
+    endDate: date,
+    comment: '',
+    payment: ''
+  }
+Order */
+
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const landlord = require('./landlord')
 
-const ReuestSchema = new Schema({
+const orderSchema = new Schema({
     Time: {
         type: String,
         required: [true, 'Time field is required'],
     },
-    UserID: {
-        type: String,
-        required: [true, 'UserID field is required'],
-    },
-    StorageID: {
-        type: String,
-        required: [true, 'StorageID field is required'],
-    }
+    landlord: landlord,
+    leaser: leaser,
+    startDate: Date,
+    endDate: Date,
+    comment: String, 
+
 })
+
+// represent a collection called Storage by MongoDB
+const order = mongoose.model('order', orderSchema);
+
+//Export it
+module.exports = order;
