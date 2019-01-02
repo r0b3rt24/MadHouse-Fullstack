b07db00f536afe74c4c395276b8311956e0788ea
Robert Cao <ch.robert.26@gmail.com>
2019-01-02 21:41:23 +0800

    comment schema

---
+++ Server/models/comment.js
@@ -0,0 +1,18 @@
+/* 
+{
+    order: '',  // id of order
+    rating: 5,  // integer from 1-5
+    comment: "", 
+    date: date,
+  }
+Comment */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    order: String,
+    rating: Number,
+    comment: String,
+    date: Date, 
+})
