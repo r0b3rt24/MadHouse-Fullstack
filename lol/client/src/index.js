bc5e284863db6b423764a3727ae905e15c45cada
Robert Cao <ch.robert.26@gmail.com>
2019-01-04 11:59:20 +0800

    remove useless files

--- lol/client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import NavBar from './NavBar'
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
