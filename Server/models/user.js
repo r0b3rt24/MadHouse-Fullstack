a13f81c962ec4d6ee4f84272e738776ec98ff81f
Robert Cao <ch.robert.26@gmail.com>
2019-01-07 19:04:26 +0800

    shecma changed

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 const landlordSchema = require("./landlord");
-// const leaserSchema = require("./leaser");
+const leaserSchema = require("./leaser");
 
 
 const userSchema = new Schema({
-    // landlord: {
-    //     type: Schema.Types.ObjectId,
-    //     ref: landlordSchema
-    // },
-    // leaser: {
-    //     type: Schema.Types.ObjectId,
-    //     ref: leaserSchema
-    // },
+    landlord: {
+        type: Schema.Types.ObjectId,
+        ref: landlordSchema
+    },
+    leaser: {
+        type: Schema.Types.ObjectId,
+        ref: leaserSchema
+    },
     googleid: String,
     username: {
         type: String,
